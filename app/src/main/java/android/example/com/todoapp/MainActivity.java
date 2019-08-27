546a3bd22d695a75a72757c93d910d3e3b89980f
Kaerah Lopez <25163211+Rhijke@users.noreply.github.com>
2019-08-26 23:27:35 -0500

    Allow user to remove to-do items

--- app/src/main/java/android/example/com/todoapp/MainActivity.java
+++ app/src/main/java/android/example/com/todoapp/MainActivity.java
@@ -2,7 +2,9 @@ package android.example.com.todoapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -29,15 +31,35 @@ public class MainActivity extends AppCompatActivity {
         // Mock data
         items.add("First");
         items.add("Second");
+
+        // Allows user to remove to-do items
+        setupListViewListener();
     }
 
     public void onAddItem(View v) {
         EditText etNewItem = findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
+
+        // Add Item to to-do list
         itemsAdapter.add(itemText);
         etNewItem.setText("");
 
         Toast.makeText(getApplicationContext(), "Item added to list.", Toast.LENGTH_SHORT).show();
+    }
 
+    private void setupListViewListener() {
+        Log.i("MainActivity", "Setting up listener on ListView.");
+
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("MainActivity", "Item removed from list at: " + position);
+
+                // Remove an item from to do list
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged(); // need to notify adapter that data changed
+                return true;
+            }
+        });
     }
 }
